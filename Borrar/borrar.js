300bdb26e0e278556e9c664aee3458ec004f61cc
BoschiAlbano <iprogramacion17@gmail.com>
2023-02-04 16:26:34 -0300

    Borrar

---
+++ Borrar/borrar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Borrar = () => {
+    return (
+        <div>
+            <h1>Hola mundo, despues Borrar</h1>
+        </div>
+        
+    );
+}
+
+export default Borrar;
